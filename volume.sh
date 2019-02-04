22f55b5993a5ea5297d2265c8f94142014c1c693
Leonardo Facundes L3onix <leonardo.fm00@gmail.com>
2019-02-04 02:54:58 -0300

    atualizado

---
+++ volume.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+VOL_VALUE=$(pactl list sinks | grep '^[[:space:]]Volume:' | \
+    head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')
+
+echo "$VOL_VALUE%"
