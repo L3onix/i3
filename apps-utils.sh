17abd7b6338f8559e9f8f2a3447730320397189f
Leonardo Facundes L3onix <leonardo.fm00@gmail.com>
2020-10-17 22:53:25 -0300

    adicionando scripts de pacotes básicos

---
+++ apps-utils.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+sudo pacman -S thunderbird \
+	thunderbird-i18n-pt-br \
+	libreoffice-still \
+	libreoffice-still-pt-br \
+	filezilla \
+	vlc \
+	blueman \
+	transmission-gtk \
+	gimp \
+	telegram-desktop \
+	gnome-calculator \
+	poppler \
+	youtube-dl \
+	sxiv \
+	moc \
+	gparted \
---
+++ base.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+sudo pacman -S xorg \
+	i3-gaps \
+	i3blocks \
+	i3status \
+	arandr \
+	dmenu \
+	rofi \
+	pcmanfm \
+	gvfs \
+	xarchiver \
+	p7zip \
+	unrar \
+	xfce4-terminal \
+	xfce4-screenshooter \
+	xfce4-notifyd \
+	xfce4-power-manager \
+	feh \
+	networkmanager \
+	network-manager-applet \
+	gsimplecal \
+	picom \
+	code \
+	vim \
+	wmctrl \
+	xdotool \
+	xorg-xinput \
+	lxinput \
+	lxappearance \
+	moka-icon-theme \
+	irssi \
+	neofetch \
+	htop \
+	polkit \
+	pulseaudio \
+	pulseaudio-alsa \
+	pavucontrol \
+
+
